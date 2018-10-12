a91dc5041d5394932ea05d63c1eb7ad12b549a5a
kotaro-sakai1941 <43873987+kotaro-sakai1941@users.noreply.github.com>
2018-10-12 17:26:30 +0900

    課題１完了

---
+++ fibonacci.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Spyderエディタ
+
+これは一時的なスクリプトファイルです
+"""
+
+def fib(n):
+    a=0
+    a1=1
+    a2=a+a1
+    a3=a1+a2
+    a4=a2+a3
+    a5=a3+a4
+    a6=a4+a5
+    a7=a5+a6
+    a8=a6+a7
+    a9=a7+a8
+    a10=a8+a9
+    return a10
+    
+print(fib(1)) 
